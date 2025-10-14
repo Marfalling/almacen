f22568f15f30c3a33ae850f311b1d4eaff77299f
Robinson <rmyanezr@gmail.com>
2025-10-14 08:45:13 -0500

    Conexión en Obras

--- _modelo/m_obras.php
+++ _modelo/m_obras.php
@@ -5,100 +5,100 @@
 
 // Mostrar todas las obras / subestaciones
 function MostrarObras() {
-    include("../_conexion/conexion_complemento.php");
+    include("../_conexion/conexion.php");
 
     $obras = [];
     $sql = "SELECT id_subestacion, nom_subestacion, act_subestacion 
-            FROM subestacion 
+            FROM {$bd_complemento}.subestacion 
             ORDER BY nom_subestacion ASC";
-    $res = mysqli_query($con_comp, $sql);
+    $res = mysqli_query($con, $sql);
     if ($res) {
         while ($row = mysqli_fetch_assoc($res)) {
             $obras[] = $row;
         }
     }
 
-    mysqli_close($con_comp);
+    mysqli_close($con);
     return $obras;
 }
 
 function MostrarObrasActivas() 
 {
-    include("../_conexion/conexion_complemento.php");
+    include("../_conexion/conexion.php");
 
     $obras = [];
     $sql = "SELECT id_subestacion, nom_subestacion, act_subestacion 
-            FROM subestacion 
+            FROM {$bd_complemento}.subestacion 
             WHERE act_subestacion = 1
             ORDER BY nom_subestacion ASC";
-    $res = mysqli_query($con_comp, $sql);
+    $res = mysqli_query($con, $sql);
     if ($res) {
         while ($row = mysqli_fetch_assoc($res)) {
             $obras[] = $row;
         }
     }
 
-    mysqli_close($con_comp);
+    mysqli_close($con);
     return $obras;
 }
 
 // Consultar obra / subestacion por ID
 function ConsultarObra($id_obra) {
-    include("../_conexion/conexion_complemento.php");
+    include("../_conexion/conexion.php");
     $id_obra = intval($id_obra);
     $obra = null;
 
     $sql = "SELECT id_subestacion, nom_subestacion, act_subestacion 
-            FROM subestacion 
+            FROM {$bd_complemento}.subestacion 
             WHERE id_subestacion = $id_obra LIMIT 1";
-    $res = mysqli_query($con_comp, $sql);
+    $res = mysqli_query($con, $sql);
     if ($res && mysqli_num_rows($res) > 0) {
         $obra = mysqli_fetch_assoc($res);
     }
 
-    mysqli_close($con_comp);
+    mysqli_close($con);
     return $obra;
 }
 
 // Registrar nueva obra / subestacion
 function RegistrarObra($nom, $est) {
-    include("../_conexion/conexion_complemento.php");
+    include("../_conexion/conexion.php");
     $nom = strtoupper(trim($nom));
 
     // Verificar duplicados
-    $sql_verif = "SELECT * FROM subestacion WHERE nom_subestacion = '$nom'";
-    $res_verif = mysqli_query($con_comp, $sql_verif);
+    $sql_verif = "SELECT * FROM {$bd_complemento}.subestacion WHERE nom_subestacion = '$nom'";
+    $res_verif = mysqli_query($con, $sql_verif);
     if ($res_verif && mysqli_num_rows($res_verif) > 0) {
-        mysqli_close($con_comp);
+        mysqli_close($con);
         return "NO";
     }
 
-    $sql = "INSERT INTO subestacion (nom_subestacion, act_subestacion) VALUES ('$nom', $est)";
-    $res = mysqli_query($con_comp, $sql);
-    mysqli_close($con_comp);
+    $sql = "INSERT INTO {$bd_complemento}.subestacion (nom_subestacion, act_subestacion) VALUES ('$nom', $est)";
+    $res = mysqli_query($con, $sql);
+    mysqli_close($con);
 
     return $res ? "SI" : "ERROR";
 }
 
 // Actualizar obra / subestacion
 function ActualizarObra($id_obra, $nom, $est) {
-    include("../_conexion/conexion_complemento.php");
+    include("../_conexion/conexion.php");
     $id_obra = intval($id_obra);
     $nom = strtoupper(trim($nom));
 
     // Verificar duplicados
-    $sql_verif = "SELECT * FROM subestacion WHERE nom_subestacion = '$nom' AND id_subestacion != $id_obra";
-    $res_verif = mysqli_query($con_comp, $sql_verif);
+    $sql_verif = "SELECT * FROM {$bd_complemento}.subestacion WHERE nom_subestacion = '$nom' AND id_subestacion != $id_obra";
+    $res_verif = mysqli_query($con, $sql_verif);
     if ($res_verif && mysqli_num_rows($res_verif) > 0) {
-        mysqli_close($con_comp);
+        mysqli_close($con);
         return "NO";
     }
 
-    $sql = "UPDATE subestacion 
+    $sql = "UPDATE {$bd_complemento}.subestacion 
             SET nom_subestacion = '$nom', act_subestacion = $est 
             WHERE id_subestacion = $id_obra";
-    $res = mysqli_query($con_comp, $sql);
-    mysqli_close($con_comp);
+    $res = mysqli_query($con, $sql);
+    mysqli_close($con);
 
     return $res ? "SI" : "ERROR";
 }
